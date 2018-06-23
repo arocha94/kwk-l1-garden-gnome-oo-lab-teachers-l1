96af0aafbcba8fe0b314627924803e40dad830f3
Alejandra Rocha <arocha94@gmail.com>
2018-06-23 16:07:13 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -13,12 +13,24 @@ class GardenGnome
     @name = name
   end 
   
+  def name 
+    @name 
+  end 
+  
   def age=(age)
     @age = age
   end 
   
+  def age 
+    @age 
+  end 
+  
   def gluten_allergy=(gluten_allergy)
     @gluten_allergy = gluten_allergy
+  end 
+  
+  def gluten_allergy
+    @gluten_allergy
   end 
   
 end 
